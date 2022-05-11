c6f4c0ad67eba64e5bf64a9303a9b11b4812f42d
Eliel Nfinic <elielmathe@nfinic.com>
2022-05-11 22:02:46 +0200

    Adding a few more libraries

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,42 @@
 #[macro_use]
 extern crate dotenv_codegen;
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpServer, Responder};
+use serde::Serialize;
+use serde::Deserialize;
 use std::env;
 use dotenv::dotenv;
 
+#[derive(Serialize)]
+struct Liability{
+    name : String,
+    value : f32
+}
 
+#[derive(Serialize)]
+struct Debt{
+    name : String,
+    value : f32
+}
 
 #[get("/liabities")]
 async fn get_liabilities() -> impl Responder{
-    
+    format!("Here are all liabilities")
 }
 
 #[post("/liabity/add")]
-async fn add_liabily() -> impl Responder{
-
+async fn add_liabily(liability : web::Json<Liability>) -> impl Responder{
+    format!("Hi ze world {:?}",liability)
 }
 
 #[get("/debts")]
 async fn get_debts() -> impl Responder{
-
+    format!("Here are all debts")
 }
 
 #[post("/debt/add")]
-async fn add_debt -> impl Responder{
-
+async fn add_debt(debt : web::Json<Debt>) -> impl Responder{
+    format!("Saving debt {:?}",debt)
 }
 
 #[actix_web::main]
